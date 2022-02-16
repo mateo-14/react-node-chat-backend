6649cf4d0a12a89b36abc1ad03abdfb05a7ace1d
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-16 20:01:30 -0300

    Remove Map and modify disconnect method

--- src/ws.js
+++ src/ws.js
@@ -7,13 +7,13 @@ function send(ws, type, payload) {
 
 module.exports.init = (server) => {
   const wss = new WebSocketServer({ server });
-  const onlineUsers = new Map();
 
   function sendAll(type, payload) {
-    onlineUsers.forEach((ws) => send(ws, type, payload));
+    wss.clients.forEach((ws) => send(ws, type, payload));
   }
 
   wss.on('connection', (ws) => {
+    console.log('New ws');
     ws.on('message', (data) => {
       try {
         const { type, payload } = JSON.parse(data);
@@ -22,19 +22,24 @@ module.exports.init = (server) => {
             .verify(payload)
             .then(({ user }) => {
               sendAll('user_connected', { user });
-              onlineUsers.set(user, ws);
+              ws.user = user;
+
               send(ws, 'auth', {
-                users: Array.from(onlineUsers.keys()).filter((_user) => _user !== user),
+                users: Array.from(wss.clients.values())
+                  .map(({ user }) => user)
+                  .filter((_user) => _user && _user !== user),
               });
+
               console.log(`${user} ws auth with token`);
+
               ws.on('close', () => {
-                onlineUsers.delete(user);
-                sendAll('user_disconnected', { user });
-                console.log(`${user} disconnected`);
+                if (!Array.from(wss.clients.values()).some(({ user }) => user === ws.user)) {
+                  sendAll('user_disconnected', { user });
+                  console.log(`${user} disconnected`);
+                }
               });
             })
             .catch(() => {
-              send(ws, 'auth', { authenticated: false });
               ws.close();
             });
         } else if (type === 'ping') {
@@ -45,5 +50,4 @@ module.exports.init = (server) => {
   });
 
   module.exports.wss = wss;
-  module.exports.onlineUsers = onlineUsers;
 };
